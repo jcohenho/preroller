26c1f7c0c1be6c2eadc1034b20f6b207eb01ab1f
Bryan Ricker <bricker88@gmail.com>
2014-02-13 10:56:05 -0800

    Remove trailing whitespace

--- app/controllers/preroller/public_controller.rb
+++ app/controllers/preroller/public_controller.rb
@@ -1,25 +1,25 @@
 module Preroller
   class PublicController < ApplicationController
     skip_before_filter :require_login
-    
-    # Handle a preroll request, delivering either a preroll file encoded in 
-    # the format asked for by the stream or an empty response if there is no 
+
+    # Handle a preroll request, delivering either a preroll file encoded in
+    # the format asked for by the stream or an empty response if there is no
     # preroll
     def preroll
       # is this output key valid?
       @output = Output.where(:key => params[:key]).first
-    
+
       if !@output
         render :text => "Invalid output key.", :status => :not_found and return
       end
-    
+
       # Valid key.  Now, are there any running campaigns?
       campaigns = @output.campaigns.active
-    
+
       # FIXME: Add path matching
       # FIXME: Add UI matching
-      
-      # This gives us the opportunity to ensure that the same 
+
+      # This gives us the opportunity to ensure that the same
       # preroll will always be returned, so that we can know
       # (more or less) what the response content-length will be.
       # If static=true, then it will return the first (most recently
@@ -29,18 +29,18 @@ module Preroller
       else
         @campaign = campaigns.any? ? campaigns[ rand( campaigns.length ) ] : nil
       end
-      
+
       if @campaign
         # Now we need to figure out how to handle the stream key they've passed us
         # key format should be (codec)-(samplerate)-(channels)-(bitrate)-(mono/stereo)
         # For instance: mp3-44100-16-64-m, aac-44100-16-48-m, etc
-      
+
         if file = @campaign.file_for_stream_key(params[:stream_key])
           # Got it... send a file
           send_file file, :disposition => 'inline' and return
         else
           render :text => "", :status => :ok and return
-        end      
+        end
       else
         render :text => "", :status => :ok and return
       end
