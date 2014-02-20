3b3076f1e52497d9ea40405309a834e1c74e8ee9
Bryan Ricker <bricker88@gmail.com>
2014-02-20 11:24:26 -0800

    Remove db directory

--- db/migrate/20120418160417_create_outputs.rb
+++
@@ -1,9 +0,0 @@
-class CreateOutputs < ActiveRecord::Migration
-  def change
-    create_table :preroller_outputs do |t|
-      t.string :key, :null => false, :max => 15, :unique => true
-      t.text :description
-      t.timestamps
-    end
-  end
-end
