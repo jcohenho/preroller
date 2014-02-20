e233480648f651b1171f47cabf48654f22335244
Bryan Ricker <bricker88@gmail.com>
2014-02-20 11:25:05 -0800

    Reference AudioEncoding module with namespace

--- spec/models/audio_encoding_spec.rb
+++ spec/models/audio_encoding_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe AudioEncoding do
+describe Preroller::AudioEncoding do
   describe '#valid' do
       it 'requires a name' do
         # This is just an example. Go write your own tests!
