ddfb25891c605894deeb8ccd98e0037dade01910
Jeremy Hoffing <jhoffing@scpr.org>
2014-02-28 16:43:42 -0800

    Refactor public controller to find an existing campaign based on the params and then send the related audio file

--- app/controllers/preroller/public_controller.rb
+++ app/controllers/preroller/public_controller.rb
@@ -6,46 +6,8 @@ module Preroller
     # the format asked for by the stream or an empty response if there is no
     # preroll
     def preroll
-      # is this output key valid?
-      @output = Output.where(:key => params[:key]).first
 
-      if !@output
-        render :text => "Invalid output key.", :status => :not_found and return
-      end
-
-      # Valid key.  Now, are there any running campaigns?
-      campaigns = @output.campaigns.active
-      context = params[:context]
-
-      # This gives us the opportunity to ensure that the same
-      # preroll will always be returned, so that we can know
-      # (more or less) what the response content-length will be.
-      # If static=true, then it will return the first (most recently
-      # created) active campaign. Otherwise, it chooses a random one.
-      if params[:consistentPreroll]
-        @campaign = campaigns.first
-      else
-        # If a specific context was given, try to find a campaign
-        # which matches that context. If no campaign matches, then
-        # we'll just select a random active campaign.
-        if context.present?
-          filtered = campaigns.select do |c|
-            c.path_filter.present? && context.match(c.path_filter)
-          end
-
-          # We now have an array of campaigns for this context.
-          # Take a random one. If the array was empty, we'll still have
-          # nil, so then fallback to a random campaign that *doesn't*
-          # specify a context.
-          # If there aren't any other ones, then we'll still have nil and
-          # no prerolls will be sent.
-          @campaign = filtered.sample
-          @campaign ||= campaigns.select { |c| c.path_filter.empty? }.sample
-        else
-          @campaign = campaigns.sample
-        end
-      end
-
+      @campaign = Campaign.find_by_key(key: params[:key], context: params[:context], consistent_preroll: params[:consistentPreroll])
       if @campaign
         # key format should be (codec)-(samplerate)-(channels)-(bitrate)-(mono/stereo)
         # For instance: mp3-44100-16-64-m, aac-44100-16-48-m, etc
--- spec/controllers/public_controller_spec.rb
+++ spec/controllers/public_controller_spec.rb
@@ -1,86 +1,21 @@
 require 'spec_helper'
 
 describe Preroller::PublicController do
-
   describe 'GET preroll' do
-
-    context 'with a valid output key and stream key' do
-      before(:each) do
-        @output = create :output, key: 'mp3'
+    context 'given a valid stream key that matches to an active campaign with an existing audio file' do
+      before do
         @valid_stream_key = 'mp3-44100-48-m'
-      end
-
-      context 'with an existing active campaign' do
-        before(:each) do
-          @campaign = create :active_campaign, output: @output
-        end
-
-        context 'with a context param' do
-          before do
-            @context = 'cool'
-            @campaign_with_context = create :active_campaign, output: @output, path_filter: @context
-            @other_campaign = create :active_campaign, output: @output, path_filter: 'bro'
-            @another_campaign = create :active_campaign, output: @output, path_filter: 'doge'
-            Preroller::Campaign.any_instance.stub(:file_for_stream_key).with(@valid_stream_key).and_return(@audio_file)
-          end
-
-          it 'selects a random campaign related to the given context' do
-            get :preroll, public_request_params(key: @output.key, stream_key: @valid_stream_key, context: @context)
-            assigns(:campaign).should eq @campaign_with_context
-          end
-        end
-
-        context 'with a consistentPreroll param' do
-          before do
-            @campaign2 = create :active_campaign, output: @output
-            @campaign3 = create :active_campaign, output: @output
-            Preroller::Campaign.any_instance.stub(:file_for_stream_key).with(@valid_stream_key).and_return(@audio_file)
-            get :preroll, public_request_params(key: @output.key, stream_key: @valid_stream_key, consistentPreroll: true)
-          end
-
-          it 'selects the first campaign' do
-            assigns(:campaign).should eq @campaign3
-          end
-        end
-
-
-        context 'with an existing audio file' do
-
-          it 'returns an audio file output' do
-            @audio_file = AUDIO_MASTER
-            Preroller::Campaign.any_instance.stub(:file_for_stream_key).with(@valid_stream_key).and_return(@audio_file)
-            get :preroll, public_request_params(key: @output.key, stream_key: @valid_stream_key)
-            assigns(:output).should eq @output
-            response.body.should eq IO.binread(@audio_file)
-          end
-        end
-
-        context 'without an audio file' do
-          it 'returns a 200 with an empty body' do
-            Preroller::Campaign.any_instance.stub(:file_for_stream_key).with(@valid_stream_key).and_return(false)
-            get :preroll, public_request_params(key: @output.key, stream_key: @valid_stream_key)
-            response.status.should eq 200
-            response.body.should eq ""
-          end
-        end
-      end
-
-      context 'without an active campaign' do
-        it 'returns a 200 with an empty body' do
-          @campaign = create :campaign, output: @output
-          get :preroll, public_request_params(key: @output.key, stream_key: @valid_stream_key)
-          response.status.should eq 200
-          response.body.should eq ""
-        end
-      end
-    end
-
-    context 'with an invalid output key' do
-      it 'returns a 404 not found' do
         @output = create :output, key: 'mp3'
-        get :preroll, public_request_params(key: 'badkey')
-        response.status.should eq 404
+        @campaign = create :active_campaign, output: @output
+      end
+
+      it 'returns the contents of the audio file of the campaign' do
+        @audio_file = AUDIO_MASTER
+        Preroller::Campaign.any_instance.stub(:file_for_stream_key).with(@valid_stream_key).and_return(@audio_file)
+        get :preroll, public_request_params(key: @output.key, stream_key: @valid_stream_key)
+        response.body.should eq IO.binread(@audio_file)
       end
     end
   end
 end
+
